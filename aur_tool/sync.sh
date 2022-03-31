07e71eb5c4d3068c2292b98fc1c9e5171a4918b5
Killer-Hacker-Oficial <killer_hacker_oficial_98@protonmail.com>
2022-03-31 12:58:52 -0600

    Upload

--- aur_tool/sync.sh
+++ aur_tool/sync.sh
@@ -4,4 +4,5 @@ DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$DIR/config"
 set -e
 
-rsync --copy-links --delete -avr "$DIR/../ctlos_dev" -avr "$DIR/../ctlos_repo" "$REMOTE"
+rsync --copy-links --delete -avr "$DIR/../ctlos_dev" -avr "$DIR/../ctlos_repo" "$KILLER"
+rsync --copy-links --delete -avr "$DIR/../ctlos_dev" -avr "$DIR/../ctlos_repo" "$CTLOS"
